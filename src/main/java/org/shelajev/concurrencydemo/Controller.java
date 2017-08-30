0f0138784effbf7d12598b33792d43dcb2af1bbf
Oleg Shelajev <shelajev@gmail.com>
2017-08-30 03:45:46 +0300

    Threads

--- src/main/java/org/shelajev/concurrencydemo/Controller.java
+++ src/main/java/org/shelajev/concurrencydemo/Controller.java
@@ -1,5 +1,7 @@
 package org.shelajev.concurrencydemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.shelajev.concurrencydemo.Misc.sleep;
+import static org.shelajev.concurrencydemo.Misc.stall;
+
 @RestController
 public class Controller {
+
+  private static final Logger log = LoggerFactory.getLogger(Controller.class);
 
   RestTemplate restTemplate;
 
@@ -18,7 +27,25 @@ public class Controller {
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public ResponseEntity<byte[]> get() {
-    byte[] image = ((Problem) urls -> restTemplate.getForObject(urls[2], byte[].class)).getFirst();
+    byte[] image = getSolution().getFirst();
     return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(image);
+  }
+
+  private Problem getSolution() {
+    return (Problem) urls -> {
+     AtomicReference<byte[]> result = new AtomicReference<>();
+
+     for(String url : urls) {
+       log.info("Fetching {}", url);
+       new Thread(()-> {
+         stall();
+         result.compareAndSet(null, restTemplate.getForObject(url, byte[].class));
+       }).start();
+     }
+     while(result.get() == null) {
+       sleep(100);
+     }
+     return result.get();
+    };
   }
 }

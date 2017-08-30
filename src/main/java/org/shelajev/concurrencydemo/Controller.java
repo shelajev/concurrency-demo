a91f508ab5ed6673f0853c0f47cbec0b3cf49f67
Oleg Shelajev <shelajev@gmail.com>
2017-08-30 04:12:02 +0300

    CompletableFutures

--- src/main/java/org/shelajev/concurrencydemo/Controller.java
+++ src/main/java/org/shelajev/concurrencydemo/Controller.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import static org.shelajev.concurrencydemo.Misc.stall;
 
@@ -31,7 +34,19 @@ public class Controller {
   }
 
   private Problem getSolution() {
-    return urls -> Arrays.asList(urls).stream().parallel().map(this::getBytes).findAny().get();
+    return urls -> {
+      CompletableFuture result = CompletableFuture.anyOf(Arrays.asList(urls).stream().map(
+        (url) -> CompletableFuture.supplyAsync(() -> getBytes(url))
+      ).collect(Collectors.toList()).toArray(new CompletableFuture[0]));
+
+      try {
+        return (byte[]) result.get();
+      }
+      catch (InterruptedException | ExecutionException e) {
+        Misc.rethrow(e);
+      }
+      return null;
+    };
   }
 
   private byte[] getBytes(String url) {

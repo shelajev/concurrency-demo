0335ac2400d8bd8cdb863052831ae09ffc8b530c
Oleg Shelajev <shelajev@gmail.com>
2017-08-30 03:45:33 +0300

    Add Misc.stall()

--- src/main/java/org/shelajev/concurrencydemo/Misc.java
+++ src/main/java/org/shelajev/concurrencydemo/Misc.java
@@ -1,10 +1,21 @@
 package org.shelajev.concurrencydemo;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Misc {
 
   static void sleep(long n) {
     try {
       Thread.sleep(n);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  static void stall() {
+    try {
+      Thread.sleep(ThreadLocalRandom.current().nextLong(500));
     }
     catch (InterruptedException e) {
       e.printStackTrace();
